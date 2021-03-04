cc36836ebc29dcf2695b32549e68bbc528b1a431
Jichao Ouyang <oyanglulu@gmail.com>
2021-03-04 17:22:12 +1100

    fix unit test

--- src/main/scala-3/us/oyanglul/dhall/generic.scala
+++ src/main/scala-3/us/oyanglul/dhall/generic.scala
@@ -2,78 +2,112 @@ package us.oyanglul.dhall
 
 import org.dhallj.ast.{Application, FieldAccess, RecordLiteral, UnionType}
 import org.dhallj.codec.Decoder.Result
-import org.dhallj.codec.{Decoder, DecodingFailure}
+import org.dhallj.codec.{Decoder => BasicDecoder, DecodingFailure, Encoder}
 import org.dhallj.core.Expr
 import scala.deriving._
 import scala.compiletime.{erasedValue, summonInline,constValueTuple}
 import org.dhallj.codec.Decoder._
 
 object generic {
-  private inline def summonAll[T <: Tuple]: List[Decoder[_]] =
-   inline erasedValue[T] match
-      case _: EmptyTuple => Nil
-      case _: (t *: ts) => summonInline[Decoder[t]] :: summonAll[ts]
-
-  given decodeHNil: Decoder[EmptyTuple] with {
-    def decode(expr: Expr): Result[EmptyTuple] = Right(EmptyTuple)
-    // visitor pattern is not use in scala
-    def isValidType(typeExpr: Expr): Boolean = true
-    def isExactType(typeExpr: Expr): Boolean = true
+  trait Decoder[T] {
+    def decode(expr: Expr): Result[T]
   }
 
-  inline def fieldNames(p: Mirror) =
-    constValueTuple[p.MirroredElemLabels].productIterator
-
-  extension (d: Decoder.type)
-    inline def derived[T](using m: Mirror.Of[T]) = _derived[T]
-  inline given _derived[T](using m: Mirror.Of[T]): Decoder[T] =
+  object Decoder {
+    given Decoder[Double] with {
+      def decode(expr: Expr) = decodeDouble.decode(expr)
+    }
+    given Decoder[String] with {
+      def decode(expr: Expr) = decodeString.decode(expr)
+    }
+    given Decoder[Long] with {
+      def decode(expr: Expr) = decodeLong.decode(expr)
+    }
+    given Decoder[Int] with {
+      def decode(expr: Expr) = decodeInt.decode(expr)
+    }
+    given Decoder[BigInt] with {
+      def decode(expr: Expr) = decodeBigInt.decode(expr)
+    }
+    given Decoder[Boolean] with {
+      def decode(expr: Expr) = decodeBoolean.decode(expr)
+    }
+    given [A:Decoder]: Decoder[List[A]] with {
+      def decode(expr: Expr) = decodeList[A].decode(expr)
+    }
+    given [A:Decoder]: Decoder[Vector[A]] with {
+      def decode(expr: Expr) = decodeVector[A].decode(expr)
+    }
+    given [A:Decoder]: Decoder[Option[A]] with {
+      def decode(expr: Expr) = decodeOption[A].decode(expr)
+    }
+    given [A:Encoder,B:Decoder]: Decoder[Function1[A,B]] with {
+      def decode(expr: Expr) = decodeFunction1[A,B].decode(expr)
+    }
+    inline def summonAll[T <: Tuple]: List[Decoder[_]] =
+    inline erasedValue[T] match
+        case _: EmptyTuple => Nil
+        case _: (t *: ts) => summonInline[Decoder[t]] :: summonAll[ts]
+  
+    given decodeHNil: Decoder[EmptyTuple] with {
+      def decode(expr: Expr): Result[EmptyTuple] = Right(EmptyTuple)
+    }
+  
+    inline def fieldNames(p: Mirror) =
+      constValueTuple[p.MirroredElemLabels].productIterator
+    def decodeProduct[T](p: Mirror.ProductOf[T], elems: => List[Decoder[_]], names: => Seq[String]): Decoder[T] =
+      new Decoder[T] {
+        def decode(expr: Expr): Result[T] =
+          expr.normalize match {
+            case RecordLiteral(fields) =>
+               val product = names.zip(elems.iterator.asInstanceOf[Iterator[Decoder[_]]]).foldRight(Right(EmptyTuple)){( n: (String, Decoder[_]), acc: Result[Tuple]) =>
+                 val (name, decoder) = n
+                 for {
+                   a <- acc
+                   valueExpr <- fields
+                    .get(name)
+                    .toRight(
+                      new DecodingFailure(s"missing key ${name} in record", expr)
+                    )
+                   value <- decoder.decode(valueExpr)
+                 } yield value *: a
+               }
+               product.map(p.fromProduct(_))
+            case FieldAccess(UnionType(_), typ) =>
+              Right(p.fromProduct(EmptyTuple))
+            case other =>
+              Left(new DecodingFailure(s"expr is not a record ${other}", other)) 
+          }
+      }
+    def decodeCoproduct[T](s: Mirror.SumOf[T], elems: => List[Decoder[_]], names: => Seq[String]): Decoder[T] =
+        new Decoder[T] {
+        def decode(expr: Expr): Result[T] = {
+          val decoderEitherLeftOrRight = (arg: Expr, typ: String) =>
+            elems(names.indexOf(typ)).decode(arg).asInstanceOf[Result[T]]
+          expr.normalize match {
+            case Application(FieldAccess(UnionType(_), typ), arg) =>
+              decoderEitherLeftOrRight(arg, typ)
+            case FieldAccess(UnionType(_), typ) =>
+              decoderEitherLeftOrRight(expr, typ)
+            case other =>
+              Left(new DecodingFailure(s"expr $other is not a union", other))
+          }
+        }
+  
+      }  
+    inline given derived[T](using m: Mirror.Of[T]): Decoder[T] =
       lazy val allDecoders = summonAll[m.MirroredElemTypes]
       lazy val names = fieldNames(m).asInstanceOf[Iterator[String]].toSeq
       inline m match
         case s: Mirror.SumOf[T] => 
           decodeCoproduct(s, allDecoders, names)
-        case p: Mirror.ProductOf[T] => decodeProduct(p,allDecoders, names)
-
-  def decodeProduct[T](p: Mirror.ProductOf[T], elems: => List[Decoder[_]], names: => Seq[String]): Decoder[T] =
-    new Decoder[T] {
-      def decode(expr: Expr): Result[T] =
-         expr.normalize match {
-           case RecordLiteral(fields) =>
-             val product = names.zip(elems.iterator.asInstanceOf[Iterator[Decoder[_]]]).foldRight(Right(EmptyTuple)){( n: (String, Decoder[_]), acc: Result[Tuple]) =>
-               val (name, decoder) = n
-               for {
-                 a <- acc
-                 valueExpr <- fields
-                  .get(name)
-                  .toRight(
-                    new DecodingFailure(s"missing key ${name} in record", expr)
-                  )
-                 value <- decoder.decode(valueExpr)
-               } yield value *: a
-             }
-             product.map(p.fromProduct(_))
-          case other =>
-            Left(new DecodingFailure("expr is not a record", other)) 
-        }
+        case p: Mirror.ProductOf[T] =>
+          decodeProduct(p,allDecoders, names)
+  }
+  implicit def dhalljDecoder[T](using d: Decoder[T]): BasicDecoder[T] = new BasicDecoder[T] {
+      def decode(expr: Expr): Result[T] = d.decode(expr)
       def isValidType(typeExpr: Expr): Boolean = true
       def isExactType(typeExpr: Expr): Boolean = true
-    }
-  def decodeCoproduct[T](s: Mirror.SumOf[T], elems: => List[Decoder[_]], names: => Seq[String]): Decoder[T] =
-    new Decoder[T] {
-      def decode(expr: Expr): Result[T] = {
-        val decoderEitherLeftOrRight = (arg: Expr, typ: String) =>
-          elems(names.indexOf(typ)).decode(expr).asInstanceOf[Result[T]]
-        expr.normalize match {
-          case Application(FieldAccess(UnionType(_), typ), arg) =>
-            decoderEitherLeftOrRight(arg, typ)
-          case FieldAccess(UnionType(_), typ) =>
-            decoderEitherLeftOrRight(expr, typ)
-          case other =>
-            Left(new DecodingFailure(s"expr $other is not a union", other))
-        }
-      }
+  }
 
-      def isValidType(typeExpr: Expr): Boolean = true
-      def isExactType(typeExpr: Expr): Boolean = true
-    }
 }
--- src/test/scala/us/oyanglul/dhall/GenericSpec.scala
+++ src/test/scala/us/oyanglul/dhall/GenericSpec.scala
@@ -8,67 +8,66 @@ import org.dhallj.codec.Decoder._
 
 import Shape._
 import Env._
-
 class GenericSpec extends FunSuite {
 
-  // test("generic decode product") {
-  //   val Right(expr) = """{width = 1, height = 1.1}""".parseExpr
-  //   val Right(decoded: Rectangle) = expr.as[Shape]
-  //   assertEquals(Rectangle(1, 1.1), decoded)
-  // }
+  test("generic decode product") {
+    val Right(expr) = """{width = 1, height = 1.1}""".parseExpr
+    val Right(decoded: Rectangle) = expr.as[Rectangle]
+    assertEquals(Rectangle(1, 1.1), decoded)
+  }
 
   test("generic decode empty product") {
     val Right(expr) = """{=}""".parseExpr
     val Right(decoded) = expr.as[Empty]
     assertEquals(decoded, Empty())
   }
-  // test("generic decode coproduct") {
-  //   val Right(expr1) =
-  //     """let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}> in Shape.Circle {radius = 1.1}""".parseExpr
-  //   val Right(expr2) =
-  //     """let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}> in Shape.Rectangle {width = 1.1, height = 1.2}""".parseExpr
-  //   val Right(decoded1) = expr1.normalize().as[Shape]
-  //   val Right(decoded2) = expr2.normalize().as[Shape]
-  //   assertEquals(decoded1, Circle(1.1))
-  //   assertEquals(decoded2, Rectangle(1.1, 1.2))
-  // }
+  test("generic decode coproduct") {
+    val Right(expr1) =
+      """let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}> in Shape.Circle {radius = 1.1}""".parseExpr
+    val Right(expr2) =
+      """let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}> in Shape.Rectangle {width = 1.1, height = 1.2}""".parseExpr
+    val Right(decoded1) = expr1.normalize().as[Shape]
+    val Right(decoded2) = expr2.normalize().as[Shape]
+    assertEquals(decoded1, Circle(1.1))
+    assertEquals(decoded2, Rectangle(1.1, 1.2))
+  }
 
-  // test("nested coproduct and product") {
-  //   val Right(expr) =
-  //     """|
-  //        |let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}>
-  //        |in {name = "Outer Class", shape = Shape.Circle {radius = 1.2}}
-  //        |""".stripMargin.parseExpr
-  //   val Right(decoded) = expr.normalize.as[OuterClass]
-  //   assertEquals(decoded, OuterClass("Outer Class", Circle(1.2)))
-  // }
+  test("nested coproduct and product") {
+    val Right(expr) =
+      """|
+         |let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}>
+         |in {name = "Outer Class", shape = Shape.Circle {radius = 1.2}}
+         |""".stripMargin.parseExpr
+    val Right(decoded) = expr.normalize.as[OuterClass]
+    assertEquals(decoded, OuterClass("Outer Class", Circle(1.2)))
+  }
 
-  // test("exception") {
-  //   val Right(expr) = "{width = \"asdf\", height = 1.1}".parseExpr
-  //   val Left(error) = expr.normalize.as[Circle]
-  //   assertEquals(
-  //     error.getMessage,
-  //     "Error decoding missing key radius in record"
-  //   )
-  // }
+  test("exception") {
+    val Right(expr) = "{width = \"asdf\", height = 1.1}".parseExpr
+    val Left(error) = expr.normalize.as[Circle]
+    assertEquals(
+      error.getMessage,
+      "Error decoding missing key radius in record"
+    )
+  }
 
-  // test("case object") {
-  //   val Right(expr) = """{env = <Local | Sit | Prod>.Sit}""".parseExpr
-  //   val Right(decoded) = expr.normalize().as[Config]
-  //   assertEquals(decoded, Config(Sit))
-  // }
+  test("case object") {
+    val Right(expr) = """{env = <Local | Sit | Prod>.Sit}""".parseExpr
+    val Right(decoded) = expr.normalize().as[Config]
+    assertEquals(decoded, Config(Sit))
+  }
 
-  // test("empty case class") {
-  //   val Right(expr) = """{env = <Local | Sit | Prod>.Sit}""".parseExpr
-  //   val Right(decoded) = expr.normalize().as[Config]
-  //   assertEquals(decoded, Config(Sit()))
-  // }
+  test("empty case class") {
+    val Right(expr) = """<Local | Sit | Prod>.Sit""".parseExpr
+    val Right(decoded) = expr.normalize().as[Env2]
+    assertEquals(decoded, Env2.Sit())
+  }
 
-  // test("list of Shape") {
-  //   val Right(expr) =
-  //     """let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}> in {shapes = [Shape.Circle {radius = 1.1}]}""".parseExpr
-  //   val Right(decoded) = expr.normalize().as[Shapes]
+  test("list of Shape") {
+    val Right(expr) =
+      """let Shape = <Rectangle: {width: Double, height: Double}| Circle: {radius: Double}> in {shapes = [Shape.Circle {radius = 1.1}]}""".parseExpr
+    val Right(decoded) = expr.normalize().as[Shapes]
 
-  //   assertEquals(decoded, Shapes(List(Circle(1.1))))
-  // }
+    assertEquals(decoded, Shapes(List(Circle(1.1))))
+  }
 }
